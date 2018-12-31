9a5cc468d1888b8198324ffdba06f929ed106ec6
christopher.tero Tero <christopher.tero@gmail.com>
2018-12-31 20:41:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else return false
-  if (string.toUpperCase() === string) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED"
   } else return false
 }
